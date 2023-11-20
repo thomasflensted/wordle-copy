9a1252a8517855989dba851dca48b066f2bdeab3
Thomas Flensted <thomasflenstedjensen@gmail.com>
2023-11-20 18:15:29 +0400

    keyboard added, game done, needs polishing

--- scripts.js
+++ scripts.js
@@ -4,42 +4,134 @@ const WORD = WORDS[Math.floor(Math.random() * WORDS.length)];
 document.addEventListener("DOMContentLoaded", () => {
 
     const keyButtons = document.getElementsByClassName("key-tile");
-    let clicks = 0;
-    let userWord = "";
+    var userWord = "";
+    var totalUserActions = 0;
 
-    for (let i = 0; i < keyButtons.length; i++) {
-        keyButtons[i].addEventListener("click", (e) => {
-            clicks++;
-            const currentTile = getCurrentTile(clicks);
-            const key = e.target.innerHTML;
-            userWord += key;
-            animateButtonClick(keyButtons[i]);
-            updateTile(currentTile, key);
-        })
-    }
+    document.body.addEventListener("keydown", function handleKeyDown(e) {
 
-    document.body.addEventListener("keydown", (e) => {
         const key = e.key.toUpperCase();
 
-        if (keyIsLetter(key)) {
+        if (keyIsLetter(key) && userWord.length < 5) {
             userWord += key;
-            clicks++;
-            const currentTile = getCurrentTile(clicks);
+            totalUserActions++;
+            const currentBoardTile = getCurrentTile(totalUserActions);
             const letterButton = getCorrectLetterButton(key, keyButtons);
             animateButtonClick(letterButton);
-            updateTile(currentTile, key);
+            updateTile(currentBoardTile, key);
         }
 
-        if (key == "BACKSPACE" && clicks > 0) {
-            const currentTile = document.getElementById(clicks);
-            currentTile.innerHTML = "";
-            animateButtonClick(currentTile)
-            userWord = userWord.substring(0, userWord.length - 1);
-            clicks--;
+        if (key == "BACKSPACE" && userWord.length > 0) {
+            const currentBoardTile = getCurrentTile(totalUserActions);
+            userWord = deleteLetterFromTileAndUpdateWord(currentBoardTile, userWord);
+            totalUserActions--;
+        }
+
+        if (key == "ENTER") {
+            if (userWord.length < 5) {
+                displayNotEnoughWarning();
+            } else {
+                const currentTile = getCurrentTile(totalUserActions);
+                const tileRow = currentTile.parentElement.children;
+                const gameWon = paintTiles(tileRow, userWord, keyButtons);
+                if (gameWon) {
+                    displayGameOverMessage("Yay! You guessed it!");
+                } else {
+                    if (totalUserActions == 25) {
+                        displayGameOverMessage("Better luck next time!");
+                    } else {
+                        userWord = "";
+                    }
+                }
+            }
         }
     })
 
+    for (let i = 0; i < keyButtons.length; i++) {
+        keyButtons[i].addEventListener("click", function handleMouseClick(e) {
+
+            const key = e.target.innerHTML;
+
+            if (e.target.id == "backspace") {
+                if (userWord.length > 0) {
+                    const currentBoardTile = document.getElementById(userWord.length);
+                    userWord = deleteLetterFromTileAndUpdateWord(currentBoardTile, userWord);
+                    totalUserActions--;
+                }
+            } else if (userWord.length < 5) {
+                userWord += key;
+                totalUserActions++;
+                const currentBoardTile = getCurrentTile(totalUserActions);
+                const letterButton = getCorrectLetterButton(key, keyButtons);
+                animateButtonClick(letterButton);
+                updateTile(currentBoardTile, key);
+            }
+        })
+    }
+
+    document.getElementById("about-btn").addEventListener("click", displayAboutDialogue);
+    document.getElementById("try-again").addEventListener("click", () => {
+        location.reload();
+    });
+
 });
+
+function displayGameOverMessage(msg) {
+    document.getElementById("game-over-message").innerHTML = msg;
+    document.getElementById("game-over-screen").style.display = "block";
+}
+
+function paintTiles(tileRow, userWord, buttons) {
+
+    var duplicateCheck = WORD;
+    var includedLetters = "";
+    for (let i = 0; i < userWord.length; i++) {
+        setTimeout(() => {
+            var currentLetter = userWord[i];
+            if (currentLetter == WORD[i]) {
+                tileRow[i].classList.add("letter-correct-spot");
+                duplicateCheck = duplicateCheck.replace(currentLetter, "");
+                includedLetters += currentLetter;
+            } else if (WORD.includes(currentLetter) && duplicateCheck.includes(currentLetter)) {
+                tileRow[i].classList.add("letter-not-correct-spot");
+                duplicateCheck = duplicateCheck.replace(currentLetter, "");
+                includedLetters += currentLetter;
+            } else {
+                tileRow[i].classList.add("letter-not-included");
+                const keyButton = getCorrectLetterButton(currentLetter, buttons);
+                if (!includedLetters.includes(currentLetter)) {
+                    keyButton.classList.add("letter-not-included");
+                }
+            }
+        }, i * 150);
+    }
+
+    if (WORD == userWord) return true;
+    return false;
+
+}
+
+function displayAboutDialogue() {
+
+    document.getElementById("about-bg").style.display = "block";
+    document.getElementById("close-btn").addEventListener("click", () => {
+        document.getElementById("about-bg").style.display = "none";
+    })
+}
+
+function displayNotEnoughWarning() {
+    document.getElementsByClassName("not-enough-container")[0].style.display = "block";
+    setTimeout(() => {
+        document.getElementsByClassName("not-enough-container")[0].style.display = "none";
+    }, 1500);
+}
+
+function deleteLetterFromTileAndUpdateWord(currentTile, word) {
+    currentTile.innerHTML = "";
+    animateButtonClick(currentTile);
+    const backSpaceTile = document.getElementById("backspace");
+    animateButtonClick(backSpaceTile);
+    return word.substring(0, word.length - 1);
+}
 
 function getCurrentTile(num) {
     return document.getElementById(num);
@@ -70,6 +162,7 @@ function animateButtonClick(button) {
 }
 
 function getCorrectLetterButton(key, buttons) {
+
     for (let i = 0; i < buttons.length; i++) {
         if (buttons[i].innerHTML == key) {
             return buttons[i];
